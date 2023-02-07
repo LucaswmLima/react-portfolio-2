4797f1ce2435de8093000ef045c3d60075e2bcae
Lucas William <lucaswilliamml@gmail.com>
2023-02-07 13:15:38 -0300

    adjusted the useState function

--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './portifolio.css'
 
-const data = [{
+const data = [[{
+  id: 1,
+  image: require('../../assets/pokedex-img.jpg'),
+  title: 'Pokedex em Javascript com  consumo de API REST',
+  github: 'https://github.com/LucaswmLima/javascript-pokedex',
+  demo: 'https://lucaswmlima.github.io/javascript-pokedex/'
+}, {
+  id: 2,
+  image: require('../../assets/weather.jpg'),
+  title: 'Clima e previsão do tempo  com  consumo de API REST',
+  github: 'https://github.com/LucaswmLima/javascript-weather-app',
+  demo: 'https://lucaswmlima.github.io/javascript-weather-app/'
+}
+,
+{
+  id: 3,
+  image: require('../../assets/tic-tac-toe-img.jpg'),
+  title: 'Jogo da velha desenvolvido em Javascript',
+  github: 'https://github.com/LucaswmLima/Javascript-Tic-Tac-Toe',
+  demo: 'https://lucaswmlima.github.io/Javascript-Tic-Tac-Toe/'
+}, {
+  id: 4,
+  image: require('../../assets/calculator-img.jpg'),
+  title: 'Calculadora em Javascript com validações de entrada',
+  github: 'https://github.com/LucaswmLima/javascript-calculator',
+  demo: 'https://lucaswmlima.github.io/javascript-calculator/'
+}
+,
+{
+  id: 5,
+  image: require('../../assets/IMC.jpg'),
+  title: 'Calculatora de IMC desenvolvida em Flutter',
+  github: 'https://github.com/LucaswmLima/javascript-calculator',
+  demo: ''
+},
+{
+  id: 6,
+  image: require('../../assets/lucy.jpg'),
+  title: 'Assistente virtual para controle de estoque em Python',
+  github: 'https://github.com/LucaswmLima/python-lucy-assistant',
+  demo: ''
+}],
+[{
   id: 1,
   image: require('../../assets/pokedex-img.jpg'),
   title: 'Pokedex em Javascript com  consumo de API REST',
@@ -39,20 +81,86 @@ const data = [{
 {
   id: 6,
   image: require('../../assets/tic-tac-toe-img.jpg'),
-  title: 'Tic-Tac-Toe in javascript',
+  title: 'Tic-Tac-Toe em jascript',
   github: 'https://github.com/LucaswmLima/Javascript-Tic-Tac-Toe',
   demo: 'https://lucaswmlima.github.io/Javascript-Tic-Tac-Toe/'
-}]
+}],
+[{
+  id: 1,
+  image: require('../../assets/clock-unlocker.jpg'),
+  title: 'Desbloqueador automático de relógios de ponto desenvolvido com Python',
+  github: 'https://github.com/LucaswmLima/python-clock-unlocker',
+  demo: ''
+}, {
+  id: 2,
+  image: require('../../assets/weather.jpg'),
+  title: 'Clima e previsão do tempo  com  consumo de API REST',
+  github: 'https://github.com/LucaswmLima/javascript-weather-app',
+  demo: 'https://lucaswmlima.github.io/javascript-weather-app/'
+}
+,
+{
+  id: 3,
+  image: require('../../assets/tic-tac-toe-img.jpg'),
+  title: 'Jogo da velha desenvolvido em Javascript',
+  github: 'https://github.com/LucaswmLima/Javascript-Tic-Tac-Toe',
+  demo: 'https://lucaswmlima.github.io/Javascript-Tic-Tac-Toe/'
+}, {
+  id: 4,
+  image: require('../../assets/calculator-img.jpg'),
+  title: 'Calculadora em Javascript com validações de entrada',
+  github: 'https://github.com/LucaswmLima/javascript-calculator',
+  demo: 'https://lucaswmlima.github.io/javascript-calculator/'
+}
+,
+{
+  id: 5,
+  image: require('../../assets/IMC.jpg'),
+  title: 'Calculatora de IMC desenvolvida em Flutter',
+  github: 'https://github.com/LucaswmLima/javascript-calculator',
+  demo: ''
+},
+{
+  id: 6,
+  image: require('../../assets/tic-tac-toe-img.jpg'),
+  title: 'Tic-Tac-Toe em jascript',
+  github: 'https://github.com/LucaswmLima/Javascript-Tic-Tac-Toe',
+  demo: 'https://lucaswmlima.github.io/Javascript-Tic-Tac-Toe/'
+}]]
+
+
 
 
 const Portifolio = () => {
+
+  const [index, setIndex] = useState(0)
+
+const PreviousPage = () => {
+  if(index > 0){
+  setIndex(index - 1)
+  }
+  console.log(index);  
+}
+
+const NextPage = () => {
+  if(index < data.length -1){
+    setIndex(index + 1)
+    }
+  console.log(index); 
+}
+  
   return (
     <section id='portifolio'>
       <h5>Meus trabalhos recentes</h5>
       <h2>Portifólio</h2>
+      <div className="portifolio__item-cta">
+                    <div onClick={PreviousPage} className='btn' target={'_blank'} rel="noreferrer">Página Anterior</div>
+                    <div onClick={NextPage} className='btn btn-primary' target={'_blank'} rel="noreferrer">Próxima Página</div>
+                  </div>
       <div className="container portifolio__container">
+      
         {
-          data.map(({ id, image, title, github, demo }) => {
+          data[index].map(({ id, image, title, github, demo }) => {
             return demo === '' ?
               (
                 <article key={id} className="portifolio__item">
@@ -83,6 +191,10 @@ const Portifolio = () => {
 
 
       </div>
+      <div className="portifolio__item-cta">
+                    <div onClick={PreviousPage} className='btn' target={'_blank'} rel="noreferrer">Página Anterior</div>
+                    <div onClick={NextPage} className='btn btn-primary' target={'_blank'} rel="noreferrer">Próxima Página</div>
+                  </div>
     </section>
   )
 }
